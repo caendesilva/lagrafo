f7015e90b02a3312fa07c271ccd12c5ea7f2f766
Caen De Silva <caen@desilva.se>
2022-04-25 14:40:48 +0200

    Remove sidebar toggle button for now

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -10,18 +10,13 @@
 </head>
 <body id="lagrafo-app">
 <nav id="navigation-menu">
-    <button id="toggle-nav">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-            <path d="M0 0h24v24H0z" fill="none"/>
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
-        </svg>
-    </button>
+    <header id="navigation-header">
+        <strong>
+            <a href="{{ Lagrafo::route('index') }}">{{ Lagrafo::appName() }}</a>
+        </strong>
+    </header>
 
-    <strong>
-        <a href="{{ Lagrafo::route('index') }}">{{ Lagrafo::appName() }}</a>
-    </strong>
-
-    <form action="" method="GET">
+    <form id="search-form" action="" method="GET">
         <label>
             <input type="text" name="search" placeholder="Search...">
         </label>
