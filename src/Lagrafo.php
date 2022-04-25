f056fadee4667828c9c18f41d277d0fdb4459400
Caen De Silva <caen@desilva.se>
2022-04-25 11:43:03 +0200

    Add caching todo

--- src/Lagrafo.php
+++ src/Lagrafo.php
@@ -2,6 +2,11 @@
 
 namespace DeSilva\Lagrafo;
 
+/**
+ * @todo Implement caching of compiled Markdown files.
+ *       can be done by saving the compiled static HTML
+ *       in the Laravel storage and proxying the request.
+ */
 class Lagrafo
 {
     // Build your next great package.
