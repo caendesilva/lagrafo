5e2db8e8dcb5e3a64459d1149a49eaeefc97c99f
Caen De Silva <caen@desilva.se>
2022-04-26 15:25:04 +0200

    Refactor code, moving actions into functions

--- dist/lagrafo.js
+++ dist/lagrafo.js
@@ -5,23 +5,22 @@
 var sidebarOpen = false;
 var sidebarToggleButton = document.getElementById('sidebar-toggle');
 var sidebar = document.getElementById('sidebar');
+var backdrop = document.createElement('div');
 function toggleSidebar() {
-    function closeSidebar() {
-        sidebarOpen = false;
-        sidebar.classList.remove('active');
-        sidebarToggleButton.classList.remove('active');
-        // Remove the backdrop
-        var backdrop = document.getElementById('sidebar-backdrop');
-        backdrop.parentNode.removeChild(backdrop);
-        document.getElementById('content').classList.remove('sidebar-active');
-        document.body.style.background = '';
-    }
+    sidebarOpen ? closeSidebar() : openSidebar();
     function openSidebar() {
         sidebarOpen = true;
         sidebar.classList.add('active');
         sidebarToggleButton.classList.add('active');
-        // Add backdrop
-        var backdrop = document.createElement('div');
+        createBackdropElement();
+    }
+    function closeSidebar() {
+        sidebarOpen = false;
+        sidebar.classList.remove('active');
+        sidebarToggleButton.classList.remove('active');
+        removeBackdropElement();
+    }
+    function createBackdropElement() {
         backdrop.id = 'sidebar-backdrop';
         backdrop.title = 'Click to close sidebar';
         backdrop.classList.add('backdrop');
@@ -31,7 +30,13 @@ function toggleSidebar() {
         document.getElementById('content').classList.add('sidebar-active');
         document.body.style.background = 'black';
     }
-    sidebarOpen ? closeSidebar() : openSidebar();
+    function removeBackdropElement() {
+        if (backdrop.parentNode) {
+            backdrop.parentNode.removeChild(backdrop);
+        }
+        document.getElementById('content').classList.remove('sidebar-active');
+        document.body.style.background = '';
+    }
 }
 // On click of sidebar toggle button
 sidebarToggleButton.addEventListener('click', function () {
--- resources/lagrafo.ts
+++ resources/lagrafo.ts
@@ -7,36 +7,46 @@ let sidebarOpen:boolean = false;
 
 const sidebarToggleButton = document.getElementById('sidebar-toggle') as HTMLButtonElement;
 const sidebar = document.getElementById('sidebar') as HTMLDivElement;
+const backdrop:HTMLDivElement = document.createElement('div');
 
 function toggleSidebar() {
-    function closeSidebar() {
-        sidebarOpen = false;
-        sidebar.classList.remove('active');
-        sidebarToggleButton.classList.remove('active');
-        // Remove the backdrop
-        const backdrop = document.getElementById('sidebar-backdrop') as HTMLDivElement;
-        backdrop.parentNode.removeChild(backdrop);
-        document.getElementById('content').classList.remove('sidebar-active');
-        document.body.style.background = '';
-    }
+    sidebarOpen ? closeSidebar() : openSidebar();
 
     function openSidebar() {
         sidebarOpen = true;
         sidebar.classList.add('active');
         sidebarToggleButton.classList.add('active');
-        // Add backdrop
-        const backdrop = document.createElement('div');
+        createBackdropElement();
+    }
+
+    function closeSidebar() {
+        sidebarOpen = false;
+        sidebar.classList.remove('active');
+        sidebarToggleButton.classList.remove('active');
+        removeBackdropElement();
+    }
+
+    function createBackdropElement() {
         backdrop.id = 'sidebar-backdrop';
         backdrop.title = 'Click to close sidebar';
         backdrop.classList.add('backdrop');
         backdrop.classList.add('active');
+
         backdrop.addEventListener('click', closeSidebar);
         document.body.appendChild(backdrop);
+
         document.getElementById('content').classList.add('sidebar-active');
         document.body.style.background = 'black';
     }
 
-    sidebarOpen ? closeSidebar() : openSidebar();
+    function removeBackdropElement() {
+        if (backdrop.parentNode) {
+            backdrop.parentNode.removeChild(backdrop);
+        }
+        document.getElementById('content').classList.remove('sidebar-active');
+        document.body.style.background = '';
+    }
+
 }
 
 // On click of sidebar toggle button
@@ -50,4 +60,3 @@ document.addEventListener('keydown', function (e) {
         toggleSidebar();
     }
 });
-
